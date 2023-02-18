f898311105b61d2e311c11787387d2ee95c79a66
ilario-moreni <ilario2001.mor@gmail.com>
2023-02-18 17:32:01 +0100

    single comic(details -- first processing)

--- resources/views/partials/single.blade.php
+++ resources/views/partials/single.blade.php
@@ -25,9 +25,9 @@
                         <div class="col-8">
                             <h2>{{ $comics[0]['title']}}</h2>
                             <div class="my-4 buy_comic bg-success bg-gradient d-flex justify-content-between">
-                                <div class='p-3 d-flex justify-content-between'>
-                                    <span>U.S. Price $19.99</span>
-                                    <span>AVAILABLE</span>
+                                <div style='width: 100%;' class='p-3 d-flex justify-content-between'>
+                                    <div>U.S. Price $19.99</div>
+                                    <div>AVAILABLE</div>
                                     </div>
                                 <select class='p-3 bg-success bg-gradient'  name="" id="">
                                     <option value="">Check Availability</option>
@@ -39,6 +39,87 @@
                             <p class='text-end'>ADVIRSEMENT</p>
                             <img class='adv_img' src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- details --}}
+    <div class="pt-4 container-fluid bg-light comic_detail_container">
+        <div class="row pb-5">
+            <div class="col">
+                <div class="px-5 container">
+                    <div class="px-5 row">
+                        <div class="col">
+                            <h4>Talent</h4>
+                            <hr>
+                            <div class='d-flex justify-content-between'>
+                                <span class='col-3'>Art by:</span>
+                                <div class='col-8'>
+                                    @foreach($comics[0]['artists'] as $key=>$artist)
+                                        <a class='font-size-12' href="#">{{$artist}}</a>,
+                                    @endforeach
+                                </div>
+                            </div>
+                            <hr>
+                            <div class='d-flex justify-content-between'>
+                                <span class='col-3'>Written by:</span>
+                                <div class='col-8'>
+                                    @foreach($comics[0]['writers'] as $key=>$writer)
+                                        <a class='font-size-12' href="#">{{$writer}}</a>,
+                                    @endforeach
+                                </div>
+                            </div>
+                            <hr>
+                        </div>
+                        <div class="col">
+                            <h4>Specs</h4>
+                            <hr>
+                            <div class='d-flex justify-content-between'>
+                                <span class='col-3'>Series:</span>
+                                <div class='col-8'>
+                                    <a href="">{{$comics[0]['series']}}</a>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class='d-flex justify-content-between'>
+                                <span class='col-3'>U.S. Price:</span>
+                                <div class='col-8'>
+                                    {{$comics[0]['price']}}
+                                </div>
+                            </div>
+                            <hr>
+                            <div class='d-flex justify-content-between'>
+                                <span class='col-3'>On Sale Date:</span>
+                                <div class='col-8'>
+                                    {{\Carbon\Carbon::createFromFormat('Y-m-d', $comics[0]['sale_date'])->format('M d Y');}}
+                                </div>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="mt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col border border-secondary p-4 justify-content-between d-flex">
+                        <div class='font-size-12 text-secondary fw-bold'>DIGITAL COMICS</div>
+                        <img class='merch_icon_1' src="{{ Vite::asset('resources/img/cta-icons.png') }}" alt="">
+                    </div>
+                    <div class="col border border-secondary p-4 justify-content-between d-flex">
+                        <div class='font-size-12 text-secondary fw-bold'>SHOP DC</div>
+                        <img class='merch_icon_2' src="{{ Vite::asset('resources/img/cta-icons.png') }}" alt="">
+                    </div>
+                    <div class="col border border-secondary p-4 justify-content-between d-flex">
+                        <div class='font-size-12 text-secondary fw-bold'>COMIC SHOP LOCATOR</div>
+                        <img class='merch_icon_1' src="{{ Vite::asset('resources/img/cta-icons.png') }}" alt="">
+                    </div>
+                    <div class="col border border-secondary p-4 justify-content-between d-flex">
+                        <div class='font-size-12 text-secondary fw-bold'>SUBSCRIPTIONS</div>
+                        <img class='merch_icon_2' src="{{ Vite::asset('resources/img/cta-icons.png') }}" alt="">
                     </div>
                 </div>
             </div>
