6e65208e2a72c426da568495a71336f889d947ec
ilario-moreni <ilario2001.mor@gmail.com>
2023-02-18 12:05:45 +0100

    single comic(description -- first processing)

--- resources/views/partials/single.blade.php
+++ resources/views/partials/single.blade.php
@@ -1,11 +1,46 @@
 @extends('layouts.app')
 @section('main')
 
-
-    <div class="container-fluid comic_container bg-primary py-5">
+{{-- blue bar --}}
+    <div class="container-fluid comic_top_container bg-primary py-5">
         <div class="row">
             <div class="col">
-                
+                <div class="container px-5">
+                    <div class="row px-5">
+                        <div class="col">
+                            <img src="{{$comics[0]['thumb']}}" alt="" class="comic_cover">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- description --}}
+    <div class="container-fluid comic_description_container py-5">
+        <div class="row">
+            <div class="col">
+                <div class="container px-5">
+                    <div class="row px-5">
+                        <div class="col-8">
+                            <h2>{{ $comics[0]['title']}}</h2>
+                            <div class="my-4 buy_comic bg-success bg-gradient d-flex justify-content-between">
+                                <div class='p-3 d-flex justify-content-between'>
+                                    <span>U.S. Price $19.99</span>
+                                    <span>AVAILABLE</span>
+                                    </div>
+                                <select class='p-3 bg-success bg-gradient'  name="" id="">
+                                    <option value="">Check Availability</option>
+                                </select> 
+                           </div>
+                           <p class='text-secondary'>{{ $comics[0]['description']}}</p>
+                        </div>
+                        <div class="adv_container col-4">
+                            <p class='text-end'>ADVIRSEMENT</p>
+                            <img class='adv_img' src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
